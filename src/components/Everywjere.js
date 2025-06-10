ed1d3294716fdb0253e3e8d12f9c532d1a0633fc
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2025-06-10 13:07:01 +0530

    making mobile for hsSSa

--- src/components/Everywjere.js
+++ src/components/Everywjere.js
@@ -24,10 +24,14 @@ function Everywhere() {
     <div className="w-full flex justify-center items-center">
       <section className="w-full px-4 py-16 bg-white">
         <div className="max-w-7xl mx-auto text-center">
+          <h1 className="uppercase text-[#d94b0f] px-8 pt-4 rounded-full text-3xl md:text-5xl font-bold mb-4 inline-block">
+            We are everywhere
+          </h1>
+          <p className="rounded-full text-3xl md:text-xl font-bold mb-12 inline-block">Widely available through top e-commerce platforms and retail outlets worldwide</p>
           <div className="grid md:grid-cols-2 gap-10 items-start justify-between">
             {/* Online Section */}
             <div>
-              <h2 className="inline-block bg-[#d94b0f] text-white px-6 py-2 rounded-full text-xl font-bold mb-6">
+              <h2 className="inline-block bg-[#d94b0f] text-white px-6 py-2 rounded-full text-xl font-bold mb-9">
                 Online
               </h2>
               <div className="grid grid-cols-3 gap-6 justify-items-center">
@@ -46,7 +50,7 @@ function Everywhere() {
 
             {/* Offline Section */}
             <div>
-              <h2 className="inline-block bg-[#d94b0f] text-white px-6 py-2 rounded-full text-xl font-bold mb-6">
+              <h2 className="inline-block bg-[#d94b0f] text-white px-6 py-2 rounded-full text-xl font-bold mb-9">
                 Offline
               </h2>
               <div className="grid grid-cols-3 gap-6 justify-items-center">
